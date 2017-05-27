ace326e818f3c1893561ae7a7594d6745f5ee4b0
Lumjing <lumjing@gmail.com>
2017-05-27 11:38:33 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,14 @@
+//Write a program to print the sum of two numbers without using + operator. Other operators have to be used
+public class Addition {
+
+	public static void main(String[] args) {
+//Variable declaration
+		int a,b,SumAdd;		
+		a=10;
+		b=20;
+		SumAdd=a-(-b);
+		System.out.print("The Sum of two numbers :"+SumAdd);
+
+	}
+
+}
